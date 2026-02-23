2bdc1c59b92b522a7d6d7f53bd929f07d674f3cc
1bpmro <ti1bpmro@gmail.com>
2026-02-23 08:18:18 -0400

    Create config.js

---
+++ core/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+        API_URL: "https://script.google.com/macros/s/AKfycbySobQVE00uUwPdlJwvfWzVgfq9N822lBjnIYkp5tMq1-pGE1GzKJHhJKsiepIDZVvSow/exec",
+        VERSAO: "5.0.1",
+        EMAIL_LIST: ["gmail.com","hotmail.com","outlook.com","yahoo.com","pm.ro.gov.br"],
+        ADMIN_EMAIL: "ti1bpmro@gmail.com"
+    };
