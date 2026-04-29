dfc38839b6e6d917136f69adecb7c5e5186bfca0
1bpmro <ti1bpmro@gmail.com>
2026-04-29 10:38:25 -0400

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDqAtLFEwpxN2Yhju8X8I0QeHWR66copLc",
+  authDomain: "derso-8294b.firebaseapp.com",
+  projectId: "derso-8294b",
+  messagingSenderId: "1056159074696",
+  appId: "1:1056159074696:web:90962abec6bf703c5d923d"
+});
+
+const messaging = firebase.messaging();
+
+// 👇 opcional: receber push em background
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/assets/icon-192.png"
+  });
+});
