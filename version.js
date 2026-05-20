3e0eba40880b181d71be6ab70ad69cbb7bf58eb4
1bpmro <ti1bpmro@gmail.com>
2026-05-20 11:41:32 -0400

    Create version.js

---
+++ version.js
@@ -0,0 +1,7 @@
+// version.js - DERSO VERSION CONTROL CENTER 🎯
+export const VERSION_CONTROL = Object.freeze({
+    SISTEMA: "9.5.7",
+    STATUS: "PRODUÇÃO",
+    COMPILACAO: "2026-05-20", // Data atualizada de maio de 2026
+    LOG_TAG: "DERSO v9.5.7 [CORE ENGINE]"
+});
